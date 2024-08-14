8fa544063b4ee30035496041ebaf53c0321caf4f
terry-ice <terryloveyan@qq.com>
2024-08-14 19:41:08 +0800

    feat: update config

--- lua/muze/plugins/comment.lua
+++ lua/muze/plugins/comment.lua
@@ -14,6 +14,22 @@ return {
     comment.setup({
       -- for commenting tsx, jsx, svelte, html files
       pre_hook = ts_context_commentstring.create_pre_hook(),
+      -- 注释操作的映射
+      toggler = {
+        line = "<leader>c", -- 切换行注释
+        block = "<leader>b", -- 切换块注释
+      },
+      -- 可视模式下的映射
+      opleader = {
+        line = "<leader>c",
+        block = "<leader>b",
+      },
+      -- Extra mappings
+      extra = {
+        above = "<leader>cO", -- 在上方插入注释
+        below = "<leader>co", -- 在下方插入注释
+        eol = "<leader>cA", -- 在行尾插入注释
+      },
     })
   end,
 }
